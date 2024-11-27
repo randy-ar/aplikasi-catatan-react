b643ffec390f070991cd08ca2973ec9def1fc5d1
randy <randy@daclen.com>
2024-11-27 15:13:00 +0700

    ubah navbar jadi judul baru

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -7,7 +7,7 @@ class NavbarLayout extends React.Component {
   render(){
     return (
       <nav className="navbar">
-        <h1>Ambasing</h1>
+        <h1>Aplikasi Ambasing untuk Member JMK 48</h1>
         <input maxLength={50} value={this.props.cari} onChange={this.props.onCariChangeHandler} placeholder="Cari catatan" type="text"/>
       </nav>
     )
